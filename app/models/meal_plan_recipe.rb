4ee9597ba754e5a934cec3a04315ed4a2895dca9
charlotte bunter <charlottebunter@gmail.com>
2025-11-25 17:48:27 +0100

    removed useless models

--- app/models/meal_plan_recipe.rb
+++
@@ -1,7 +0,0 @@
-class MealPlanRecipe < ApplicationRecord
-  belongs_to :meal_plan
-  belongs_to :recipe
-
-  # Optionnel : type de repas (breakfast/lunch/dinner)
-  validates :meal_type, presence: true
-end
