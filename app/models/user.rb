07aae010f337b31dc22ba32b068b1a9168ddf106
Josephine Katz-Trataris <josephine.katztrataris@gmail.com>
2025-11-27 14:45:06 +0100

    clean comments user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,5 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :recipes, dependent: :destroy
   has_many :meal_plans, dependent: :destroy
-
-
-  private
-
-  # def create_profile
-  #   Profile.create!(
-  #     user: self,
-  #     gender: 'Other',
-  #     age: 18,
-  #     activity_level: 'Sedentary',
-  #     weekly_budget_max: 0.0,
-  #     max_prep_time_minutes: 60,
-  #     allergies: ''
-  #   )
-  # end
+  
 end
